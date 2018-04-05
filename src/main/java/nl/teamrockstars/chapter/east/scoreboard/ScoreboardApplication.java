5e854dc257fa7ad6e6b3fd6285bfd23ce2669313
Hans Lemkes <hanslemkes@gmail.com>
2018-04-05 20:31:13 +0200

    init commit start.spring.io

---
+++ src/main/java/nl/teamrockstars/chapter/east/scoreboard/ScoreboardApplication.java
@@ -0,0 +1,12 @@
+package nl.teamrockstars.chapter.east.scoreboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoreboardApplication.class, args);
+	}
+}
---
+++ src/test/java/nl/teamrockstars/chapter/east/scoreboard/ScoreboardApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.teamrockstars.chapter.east.scoreboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScoreboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
