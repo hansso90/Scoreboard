c953990f6d45cac44ad9b8c70d0bb9359d952f2e
Hans <hans.lemkes@kadaster.nl>
2018-04-16 09:38:33 +0200

    - fix right enum removed unused rights.

--- src/main/java/nl/teamrockstars/chapter/east/scoreboard/model/Right.java
+++ src/main/java/nl/teamrockstars/chapter/east/scoreboard/model/Right.java
@@ -4,15 +4,9 @@ import org.springframework.security.core.GrantedAuthority;
 
 public enum Right implements GrantedAuthority {
 
-    ROLE_INLOGGEN("ROLE_INLOGGEN"),
-
     ROLE_ROLEMANAGEMENT("ROLE_ROLEMANAGEMENT"),
 
-    ROLE_USER_CREATE("ROLE_USER_CREATE"),
-
-    ROLE_USER_MODIFY("ROLE_USER_MODIFY"),
-
-    ROLE_USER_DELETE("ROLE_USER_DELETE");
+    ;
 
     private String authority;
 
